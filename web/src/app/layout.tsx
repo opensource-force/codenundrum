7a9327abc6a9ec1c72de62a03a6cd1fff0d11a74
akpi816218 <gh@akpi.is-a.dev>
2024-03-12 19:02:02 -0700

    analytics

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 import { Providers } from './providers';
 import { Inter } from '../fonts';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
 	title: {
@@ -48,6 +49,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 		<html lang="en-US">
 			<body className={`dark ${Inter.className}`}>
 				<Providers>{children}</Providers>
+				<Analytics />
 			</body>
 		</html>
 	);
